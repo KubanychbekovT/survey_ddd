bbc77640d2c47ff54be801942ffb83a3342ce79a
Temirlan <brucewaynetemir@gmail.com>
2023-05-31 13:54:34 +0600

    feat: implement ui search and filter

--- lib/presentation/survey/survey_overview_page.dart
+++ lib/presentation/survey/survey_overview_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:survey/application/survey/survey_filter/survey_filter_cubit.dart';
+import 'package:survey/application/survey/survey_searcher/survey_searcher_cubit.dart';
 import 'package:survey/application/survey/survey_watcher/survey_watcher_cubit.dart';
 import 'package:survey/domain/survey/survey.dart';
+import 'package:survey/presentation/core/enums/survey_filter_enum.dart';
 import 'package:survey/presentation/core/widgets/custom_progress_indicator.dart';
 import 'package:survey/presentation/core/widgets/custom_scaffold.dart';
 import 'package:survey/presentation/core/widgets/new_survey_form.dart';
@@ -13,39 +16,92 @@ class SurveyOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      title: "Surveys",
-      body: Column(
-        children: [
-          BlocBuilder<SurveyWatcherCubit, SurveyWatcherState>(
-            builder: (context, state) {
-              return state.map(
-                  initial: (_) => SizedBox(),
-                  loadInProgress: (_) => CustomProgressIndicator(),
-                  loadSuccess: (state) {
-                    final surveys = state.surveys;
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        final survey = surveys[index];
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SurveyCard(
-                            survey: survey,
-                          ),
+    return BlocProvider<SurveyFilterCubit>(
+      create: (context) => SurveyFilterCubit(),
+      child: BlocProvider(
+        create: (context) => SurveySearcherCubit(),
+        child: CustomScaffold(
+          title: "Surveys",
+          body: Column(
+            children: [
+              _buildSearchBar(context),
+              _buildFilterOptions(context),
+              SizedBox(height: 16),
+              BlocBuilder<SurveyWatcherCubit, SurveyWatcherState>(
+                builder: (context, state) {
+                  return state.map(
+                      initial: (_) => SizedBox(),
+                      loadInProgress: (_) => CustomProgressIndicator(),
+                      loadSuccess: (state) {
+                        final surveys = state.surveys;
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            final survey = surveys[index];
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SurveyCard(
+                                survey: survey,
+                              ),
+                            );
+                          },
+                          itemCount: surveys.length,
                         );
                       },
-                      itemCount: surveys.length,
-                    );
-                  },
-                  loadFailure: (_) => SizedBox());
-            },
+                      loadFailure: (_) => SizedBox());
+                },
+              ),
+            ],
           ),
-        ],
+          onNewSurveyPressed: () => _createNewSurvey(context),
+        ),
       ),
-      onNewSurveyPressed: ()=> _createNewSurvey(context),
     );
   }
+
+  Widget _buildSearchBar(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Search Surveys',
+          prefixIcon: Icon(Icons.search),
+        ),
+        onChanged: (value) {
+          context.read<SurveySearcherCubit>().searchSurveys(surveys, value);
+        },
+      ),
+    );
+  }
+
+  Widget _buildFilterOptions(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            context.read<SurveyFilterCubit>().changeFilter(SurveyFilterType.my);
+            },
+          child: Text('My Surveys'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+                Color(0xff9e71e7)),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            context.read<SurveyFilterCubit>().changeFilter(SurveyFilterType.all);
+          },
+          child: Text('All Surveys'),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Color(0xff9e71e7))
+          ),
+        ),
+      ],
+    );
+  }
+
   void _createNewSurvey(BuildContext context) {
     Navigator.push(
       context,
@@ -53,5 +109,5 @@ class SurveyOverviewPage extends StatelessWidget {
         builder: (context) => SurveyFormPage(),
       ),
     );
-}
+  }
 }
